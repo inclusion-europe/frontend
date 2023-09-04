958e696eee27e029da109223037bca1219b2f1cd
Christophe Leroy <elroy@live.be>
2023-09-04 22:13:37 +0200

    fix(login): fix cookie set fn call

--- src/pages/Admin/index.vue
+++ src/pages/Admin/index.vue
@@ -102,8 +102,9 @@ export default {
         let expires = new Date(new Date() + 2 * 60 * 60 * 1000);
 
         this.$cookies.set("im_auth_token", res.data.access_token, { expires });
+        console.log(this.$cookies.get("im_auth_token"));
 
-        this.$router.push("/admin");
+        // this.$router.push("/admin");
       })
       .catch((err) => {
         console.error({ err });
