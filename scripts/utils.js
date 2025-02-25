8ed2231f5d558fb60177628e80547229bc3796b9
Kyng <elroy@live.be>
2025-02-25 14:44:08 +0100

    add youtube link embedder to editor + button to remove article picture

--- scripts/utils.js
+++ scripts/utils.js
@@ -119,4 +119,11 @@ utils.indicatorEvolution = (
 utils.formatDate = (unixTime) =>
   new Intl.DateTimeFormat('fr').format(new Date(unixTime));
 
+utils.youtubeParser = (url) => {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
+};
+
 export default utils;
